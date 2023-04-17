3a803afb51165ea4e285e678143f6a6d81c5f901
Pranjal Sharma <127693503+code-prsh@users.noreply.github.com>
2023-04-17 22:41:25 +0530

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,14 @@
+// Add validation to password fields
+var password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirm_password");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords do not match.");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
